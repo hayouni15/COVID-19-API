f42c66b8ee231271dfe6ee04d7a9ccbf584795e3
abdessattar hayouni <hayouniabdessattar@gmail.com>
2020-03-20 14:37:33 -0400

    added index page to public API

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Get_all = require('./utils').Get_all
 const app = express()
 
 const port = process.env.PORT || 3000
+app.set('view engine', 'hbs')
 
 app.get('/', function (req, res) {
 
@@ -10,23 +11,30 @@ app.get('/', function (req, res) {
     const country = req.query.country
     const date = req.query.date
 
-    console.log(req.query)
+    console.log(Object.keys(req.query).length)
+    if (Object.keys(req.query).length===0){
+        res.render('covid')
+
+    }
+    else{
+        msg = Get_all(category, country, (msg) => {
+            msg = JSON.parse(msg)
+            if (msg.length > 0) {
+                console.log('Data collected successfully')
+                res.send(msg)
+            }
+            else {
+                console.log('No data was found')
+                res.send({
+                    error: 'No data was found. Make sure you have access to the internet!'
+                })
+            }
+    
+        })
+    }
 
 
-    msg = Get_all(category, country, (msg) => {
-        msg = JSON.parse(msg)
-        if (msg.length > 0) {
-            console.log('Data collected successfully')
-            res.send(msg)
-        }
-        else {
-            console.log('No data was found')
-            res.send({
-                error: 'No data was found. Make sure you have access to the internet!'
-            })
-        }
 
-    })
 
 })
 
